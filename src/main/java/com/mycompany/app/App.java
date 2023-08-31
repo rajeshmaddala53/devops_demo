2003c2f53ddab70f03aa4cd5e127cdf1c78f81ff
Maddala Rajesh Kumar <132780213+rajeshmaddala53@users.noreply.github.com>
2023-08-31 15:20:00 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome To DIET College!This is a school");
+        System.out.println( "This is a school");
     }
 }
